51d8662579dc6d577cb999260289d7ea1f8277ff
Krushon <email5000@yandex.ru>
2020-02-19 16:33:14 +0300

    ver2

--- Asterisk/Asterisk SIP/asterisk.trunk-with-name.sh
+++ Asterisk/Asterisk SIP/asterisk.trunk-with-name.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Получаем количество всех транков в системе
-total=`asterisk -rx 'sip show registry' | sed -n '/registrations/p' | awk '{print $1}'`
+total=`sudo asterisk -rx 'sip show registry' | sed -n '/registrations/p' | awk '{print $1}'`
 # Получаем число активных транков
-active=`asterisk -rx 'sip show registry' | sed -n '/Registered/p' | wc -l`
+active=`sudo asterisk -rx 'sip show registry' | sed -n '/Registered/p' | wc -l`
 # Получаем имена транков с проблемам
-offline=`asterisk -rx 'sip show registry' | sed -n '/Request\|Rejected\|Authentication\|Auth/p' | awk '{print $3}'`
+offline=`sudo asterisk -rx 'sip show registry' | sed -n '/Request\|Rejected\|Authentication\|Auth/p' | awk '{print $3}'`
 # Сравниваем общее число с числом активных транков и выводим сообщение об их состоянии
 if [ $active -lt $total ]
 then
