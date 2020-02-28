022fb9d99a45a6d184d8c8dd41dacd2c3671cbcc
Krushon <email5000@yandex.ru>
2020-02-28 15:58:19 +0300

    Скрипт проверки регистрации

--- Asterisk/Grandstream/grandstream1.sh
+++ Asterisk/Grandstream/grandstream1.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-phoneip=$1
-# Проверка на ввод ip-адреса
-if [ -z "$1" ]
-	then
-		echo -en "No value set. Syntax: grandstream1.sh 192.168.0.1\n"
-		exit 1
-fi
+
+# Проверка на ввод ip-адреса в ручном режиме
+#if [ -z "$1" ]
+#	then
+#		echo -en "No value set. Syntax: grandstream1.sh 192.168.0.1\n"
+#		exit 1
+#fi
+
+GRNDSTRM1="$1"
 # Указываем пароль от телефона
 passcode="admin"
 
-url="http://$phoneip/cgi-bin/api-get_phone_status?passcode=$passcode"
+url="http://$GRNDSTRM1/cgi-bin/api-get_phone_status?passcode=$passcode"
 status=`wget $url -q -O - | awk '{print $3}' | awk -F\" '{print $2}'`
 
 if [[ $status = "available" || $status = "busy" ]]
